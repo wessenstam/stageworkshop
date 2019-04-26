eff26e0c51e8597667375b9cb0e214e4bf7b8afc
Willem Essenstam <wessenstam@gmail.com>
2019-04-26 17:58:35 +0200

    Update lib.pe.sh

--- scripts/lib.pe.sh
+++ scripts/lib.pe.sh
@@ -236,51 +236,10 @@ function cluster_check() {
   local    _test_exit
   local CURL_HTTP_OPTS=' --max-time 25 --silent --header Content-Type:application/json --header Accept:application/json  --insecure '
 
-  # shellcheck disable=2206
-  #_pc_version=(${PC_VERSION//./ })
-
-  #if (( ${_pc_version[0]} >= 5 && ${_pc_version[1]} >= 10 )); then
-  #  log "PC>=5.10, checking multicluster state..."
-  #  while true ; do
-  #    (( _loop++ ))
-
-  #    _test=$(ncli --json=true multicluster get-cluster-state | jq -r .data[0].clusterDetails.multicluster)
-  #    _test_exit=$?
-  #    log "Cluster status: |${_test}|, exit: ${_test_exit}."
-
-  #    if [[ ${_test} != 'true' ]]; then
-  #           _test=$(ncli multicluster add-to-multicluster \
-  #       external-ip-address-or-svm-ips=${PC_HOST} \
-  #        username=${PRISM_ADMIN} password=${PE_PASSWORD})
-  #      _test_exit=$?
-  #      log "Manual join PE to PC = |${_test}|, exit: ${_test_exit}."
-  #    fi
-
-  #         _test=$(ncli --json=true multicluster get-cluster-state | \
-  #                 jq -r .data[0].clusterDetails.multicluster)
-  #    _test_exit=$?
-  #    log "Cluster status: |${_test}|, exit: ${_test_exit}."
-
-  #   if [[ ${_test} == 'true' ]]; then
-  #      log "PE to PC = cluster registration: successful."
-  #      return 0
-  #    elif (( ${_loop} > ${_attempts} )); then
-  #      log "Warning ${_error} @${1}: Giving up after ${_loop} tries."
-  #      return ${_error}
-  #    else
-  #      log "@${1} ${_loop}/${_attempts}=${_test}: sleep ${_sleep} seconds..."
-  #     sleep ${_sleep}
-  #    fi
-  #  done
-  #fi
-  
-  #if (( ${_pc_version[0]} -ge 5 && ${_pc_version[1]} -eq 8 )); then
-    log "PC is version 5.8, enabling and checking"
-    # Enable the PE to PC registration
-    _json_data="{\"ipAddresses\":[\"${PC_HOST}\"],\"username\":\"${PRISM_ADMIN}\",\"password\":\"${PE_PASSWORD}\",\"port\":null}"
-    _response=$(curl -X POST $CURL_HTTP_OPTS --user ${PRISM_ADMIN}:${PE_PASSWORD} https://localhost:9440/PrismGateway/services/rest/v1/multicluster/add_to_multicluster -d $_json_data | jq '.value')
-  #fi
-
+  log "PC is version 5.8, enabling and checking"
+   # Enable the PE to PC registration
+   _json_data="{\"ipAddresses\":[\"${PC_HOST}\"],\"username\":\"${PRISM_ADMIN}\",\"password\":\"${PE_PASSWORD}\",\"port\":null}"
+   _response=$(curl -X POST $CURL_HTTP_OPTS --user ${PRISM_ADMIN}:${PE_PASSWORD} https://localhost:9440/PrismGateway/services/rest/v1/multicluster/add_to_multicluster -d $_json_data | jq '.value')
 }
 
 ###############################################################################################################################################################################
