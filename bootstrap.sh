dbe7da2a193f3a55586a2dac7bda8140f6b6a4a8
Nathan C <jncox@users.noreply.github.com>
2019-05-17 12:05:47 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -123,7 +123,7 @@ fi
     PE_HOST=${PE_HOST} \
 PRISM_ADMIN=${PRISM_ADMIN} \
 PE_PASSWORD=${PE_PASSWORD} \
-bash -x ./stage_workshop.sh -f ${MY_WORKSHOP} > stagedebug.log # \
+./stage_workshop.sh -f ${MY_WORKSHOP} # \
 #  && popd || exit
 
 echo -e "\n    DONE: ${0} ran for ${SECONDS} seconds."
