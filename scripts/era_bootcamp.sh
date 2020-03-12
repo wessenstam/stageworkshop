a7c4f3bedc6c4c27bf940777e0adf791f6408f8f
Nathan Cox <jncox@users.noreply.github.com>
2020-03-12 07:56:15 -0700

    Updated Era Vesion

--- scripts/era_bootcamp.sh
+++ scripts/era_bootcamp.sh
@@ -76,7 +76,7 @@ case ${1} in
     export OBJECTS_NW_END="${IPV4_PREFIX}.21"
 
     export QCOW2_IMAGES=(\
-      ERA-Server-build-1.2.0.1.qcow2 \
+      ERA-Server-build-1.2.1.qcow2 \
       MSSQL-2016-VM.qcow2 \
       Windows2016.qcow2 \
       CentOS7.qcow2 \
