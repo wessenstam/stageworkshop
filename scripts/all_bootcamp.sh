79eef88905eae7585405205cba41bda19f0f283e
Nathan Cox <jncox@users.noreply.github.com>
2020-05-18 13:54:50 -0700

    Update all_bootcamp.sh

--- scripts/all_bootcamp.sh
+++ scripts/all_bootcamp.sh
@@ -153,6 +153,7 @@ case ${1} in
     && pc_project \
     && object_store \
     && karbon_image_download \
+    && priority_images \
     && flow_enable \
     && pc_cluster_img_import \
     && configure_era \
