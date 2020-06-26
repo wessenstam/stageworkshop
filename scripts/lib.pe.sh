db711872bd45dd6e045c10aacf728a5ae03f4b60
Willem Essenstam <wessenstam@gmail.com>
2020-06-26 17:09:24 +0200

    Update lib.pe.sh
    updated the PE registration routine

--- scripts/lib.pe.sh
+++ scripts/lib.pe.sh
@@ -774,6 +774,26 @@ function cluster_check() {
    # Enable the PE to PC registration
    _json_data="{\"ipAddresses\":[\"${PC_HOST}\"],\"username\":\"${PRISM_ADMIN}\",\"password\":\"${PE_PASSWORD}\",\"port\":null}"
    _response=$(curl -X POST $CURL_HTTP_OPTS --user ${PRISM_ADMIN}:${PE_PASSWORD} https://localhost:9440/PrismGateway/services/rest/v1/multicluster/add_to_multicluster -d $_json_data | jq '.value')
+   
+   # Let's sleep a few seconds before moving on
+   sleep 5
+   
+   # Checking if the registration has worked
+   _pc_ip_addr=$(curl $CURL_HTTP_OPTS --user ${PRISM_ADMIN}:${PE_PASSWORD} https://localhost:9440/PrismGateway/services/rest/v1/multicluster/cluster_external_state | jq '.[].clusterDetails.ipAddresses[0]' | tr -d \")
+   while [[ -z $_pc_ip_addr ]]
+   do
+      log "Registering PE has failed, retrying"
+      curl -X POST $CURL_HTTP_OPTS --user ${PRISM_ADMIN}:${PE_PASSWORD} https://localhost:9440/PrismGateway/services/rest/v1/multicluster/add_to_multicluster -d $_json_data
+      (( _loop++ ))
+      _pc_ip_addr=$(curl $CURL_HTTP_OPTS --user ${PRISM_ADMIN}:${PE_PASSWORD} https://localhost:9440/PrismGateway/services/rest/v1/multicluster/cluster_external_state | jq '.[].clusterDetails.ipAddresses[0]' | tr -d \")
+      sleep 5
+      log "Sleeping for 5 seconds before retrying...$_loop/$_attempts"
+      if [[ $_loop -gt $_attempts ]]; then
+        log "We have tried 10 times and the cluster is not able to register... Exiting the script!!"
+        exit 1
+      fi
+   done
+   log "PE has been registered to PC... Proceeding..."
 }
 
 ###############################################################################################################################################################################
